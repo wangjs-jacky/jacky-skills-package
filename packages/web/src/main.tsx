d20cfef02d3c8dce773959d80c53f1a1cfa81447
wangjiasheng <wangjiasheng@trip.com>
2026-03-05 04:09:36 +0800

    feat: 更新前端初始化逻辑以支持 Tauri

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './styles/globals.css'
+import { initApi } from './api'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-)
+async function bootstrap() {
+  await initApi()
+
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  )
+}
+
+bootstrap()
